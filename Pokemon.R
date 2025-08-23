aa5f135459a89b8a70076bcac457ae21686fa731
tipusultan13 <156308034+tipusultan13@users.noreply.github.com>
2025-08-23 20:58:46 +0200

    Initial commit

---
+++ Pokemon.R
@@ -0,0 +1,20 @@
+
+
+# Name: Tipu Sultan #
+# This is a fun project to analyse the pokemon. #
+################################################
+
+#### Set directory and load dataset ####
+########################################
+
+setwd("/Users/tipusultan/Documents/GitHub/PokemonAnalysis")
+
+Pokemon = read.csv("pokemon.csv", header = TRUE)
+#Pokemon = read.csv(file.choose())
+View(Pokemon)
+
+
+#### Play around with the data ####
+###################################
+
+colnames(Pokemon)
